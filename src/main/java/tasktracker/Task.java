ed99a3fc6dc157e8b29dc9bcecca032108a4cec7
Eric <irrationalnarwhale@gmail.com>
2023-11-30 18:48:16 -0800

    Implemented many functions for Task

--- src/main/java/tasktracker/Task.java
+++ src/main/java/tasktracker/Task.java
@@ -2,20 +2,90 @@ package tasktracker;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.Time;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class Task {
+// TODO mention only size and startTime needs to be optional since all others are just empty 
     private String name;
-    private Size size;
+    private Optional<SizeEnum> size; 
     private ArrayList<String> description;
-    private ArrayList<Time> runTimes;
-    private Boolean isRunning;
-    private Time mostRecentStartTime;
+    private ArrayList<Duration> runTimes;
+    private Optional<LocalDateTime> mostRecentStartTime;
 
-    public void start(Time t) {
-        mostRecentStartTime = t;
+    public Task(String name) {
+        this.name = name;
+        this.description = new ArrayList<String>();
+        this.runTimes = new ArrayList<Duration>();
+        this.mostRecentStartTime = Optional.empty();
+    }
+
+    public void start(LocalDateTime t) {
+        mostRecentStartTime = Optional.of(t);
+    }
+
+    public void stop(LocalDateTime t) {
+        Duration runTime = Duration.between(mostRecentStartTime.get(), t);
+        if(runTime.isNegative()) {
+            System.out.printf(
+                "Error: End time %s is before start time %s for task %s", 
+                mostRecentStartTime.toString() , t.toString(), name);
+        }
+        runTimes.add(runTime);
+        mostRecentStartTime = Optional.empty();
+    }
+
+    public void setSize(SizeEnum size) {
+        this.size = Optional.of(size);
+    }
+
+    public String getSizeString() {
+        if(!size.isPresent()) {
+            return "Unspecified";
+        }
+        return size.get().toString();
+    }
+
+    public void addToDescription(String sentence) {
+        description.add(sentence);
+    }
+
+    public String getDescriptionString() {
+        return String.join("\n", description);
+    }
+
+    public void rename(String newName) {
+        this.name = newName;
+    }
+
+    public Duration getTotalTime() {
+        if(runTimes.size()==0) {return Duration.ZERO;}
+        Duration total = Duration.ZERO;
+        for(Duration d : runTimes) {
+            total.plus(d);
+        }
+        return total;
+    }
+
+    public Duration getMinTime() {
+        if(runTimes.size()==0) {return Duration.ZERO;}
+        return Collections.min(runTimes);
+    }
+
+    public Duration getMaxTime() {
+        if(runTimes.size()==0) {return Duration.ZERO;}
+        return Collections.min(runTimes);
+    }
+
+    public Duration getAvgTime() {
+        if(runTimes.size()==0) {return Duration.ZERO;}
+        Duration total = getTotalTime();
+        Duration average = total.dividedBy(runTimes.size()); 
+        return average;
     }
 
     public static ArrayList<Task> createTasksFromLog() {
