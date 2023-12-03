7febf02168ec9394e4838778e5a67fb511eab2f5
Eric <irrationalnarwhale@gmail.com>
2023-12-02 17:50:01 -0800

    Implemented rename from log

--- src/main/java/tasktracker/Rename.java
+++ src/main/java/tasktracker/Rename.java
@@ -1,6 +1,7 @@
 package tasktracker;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Rename implements Command{
 
@@ -15,26 +16,46 @@ public class Rename implements Command{
     }
 
     @Override
-    public void checkForErrors(String[] args) {
+    public void checkForParseErrors(String[] args) {
         checkForValidNumberOfArguments(args);
-        checkTaskExists();
-        checkNewTaskNameIsAvailable();
-    }
-    
-    private void checkNewTaskNameIsAvailable() {
-        return; //TODO implement after task stuff
     }
 
-private void checkTaskExists() {
-        return; //TODO implement after doing task stuff
-    }
-
- private void checkForValidNumberOfArguments(String[] args) {
+    private void checkForValidNumberOfArguments(String[] args) {
         if(args.length != 3) {
             System.out.println(
                 "Error: incorrect number of arguments\n" +
                 "Usage: java TM.java rename <old task name> <new task name>");
             throw new IllegalArgumentException();
         }
+    }
+
+    @Override
+    public void alterTasks(String logLine, ArrayList<Task> tasks) {
+        String[] tokenizedByRename = logLine.split(" ");
+        String oldName = tokenizedByRename[2];
+        String newName = tokenizedByRename[3];
+        checkNewTaskNameIsAvailable(newName,tasks);
+        Task taskToRename = getTaskByName(oldName, tasks);
+        taskToRename.rename(newName);
+    }
+
+    private void checkNewTaskNameIsAvailable(
+                    String newName, ArrayList<Task> tasks) {
+        for(Task t: tasks) {
+            if(t.getName().equals(newName)) {
+                System.out.printf("Error: %s is already a task. "+
+                "Please choose another name", newName);
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    @Override
+    public void performCommand(String[] args, ArrayList<Task> tasks) {
+        String oldName = args[1];
+        String newName = args[2];
+        checkNewTaskNameIsAvailable(newName,tasks);
+        Task taskToRename = getTaskByName(oldName, tasks);
+        taskToRename.rename(newName);
     }
 }
